56094b3716f9c2c1cbca91e8f4db6fe24378623f
Thibault Hallouin <thibault.hallouin@inrae.fr>
2025-02-27 16:59:30 +0100

    use pandas's future stack implementation

--- evalhyd/vigicrues/read/xml.py
+++ evalhyd/vigicrues/read/xml.py
@@ -61,7 +61,7 @@ def convert_frc_df_to_arr(df: pd.DataFrame) -> np.ndarray:
     df = df.pivot(columns='date validité', values='valeur')
 
     # append column index as additional level in row index
-    df = df.stack(level='date validité', dropna=False)
+    df = df.stack(level='date validité', future_stack=True)
 
     # determine shape of array (sites, leadtimes, members, time)
     shape = tuple(map(len, df.index.levels))
