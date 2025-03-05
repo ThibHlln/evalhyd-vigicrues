365bdc42c1b9e60b5a598ed741e7f5b37257cc09
Thibault Hallouin <thibault.hallouin@inrae.fr>
2025-03-05 12:03:57 +0100

    drop dependency on `pyspc` and read PRV files directly with `pandas`

--- evalhyd/vigicrues/read/prv.py
+++ evalhyd/vigicrues/read/prv.py
@@ -1,6 +1,6 @@
 import pandas as pd
+import io
 from typing import List
-from pyspc.io.prv import read_prv
 
 
 def read_prd_from_prv(prv_files: List[str], datatype: str) -> pd.DataFrame:
@@ -9,9 +9,15 @@ def read_prd_from_prv(prv_files: List[str], datatype: str) -> pd.DataFrame:
 
     :Paramètres:
 
-        xml_files: `list`
+        prv_files: `list`
             La liste de fichiers au format PRV contenant les prédictions
             de débits.
+
+        datatype: `str`
+            Le type de données fournies. Il peut être défini soit
+            comme ``'ensemble'`` (quand les fichiers PRV contiennent
+            des scénarios) ou ``'tendance'`` (quand les fichiers PRV
+            contiennent des tendances).
 
     :Retourne:
 
@@ -22,7 +28,7 @@ def read_prd_from_prv(prv_files: List[str], datatype: str) -> pd.DataFrame:
 
     Récupérer les prédictions de débits sous forme de dataframe :
 
-    >>> df = read_prd_from_prv(['data/GRP_B_20241211_1023_5304.prv'])
+    >>> df = read_prd_from_prv(['data/GRP_B_20241211_1023_5304.prv'], datatype='ensemble')
     >>> df.xs('K0045510', level='entités', drop_level=False).xs('0001', level='membres', drop_level=False)
                                                             valeur
     entités  échéances       membres date validité
@@ -39,37 +45,72 @@ def read_prd_from_prv(prv_files: List[str], datatype: str) -> pd.DataFrame:
              5 days 00:00:00 0001    2024-12-16 10:00:00   0.852
     [120 rows x 1 columns]
     """
+    # check declared datatype
+    if datatype not in ('ensemble', 'tendance'):
+        raise ValueError(
+            "'datatype' doit être 'ensemble' ou 'tendance'"
+        )
+
     df1 = None
 
     for prv_file in prv_files:
-        # read in PRV file with `pyspc`
-        s = read_prv(prv_file, datatype=datatype)
+        # read in PRV file as text
+        with open(prv_file, 'r') as f:
+            txt = f.read()
 
-        # retrieve dataframe from `pyspc.Series`
-        df0 = s.concat()
+            # uncomment relevant lines for creation of dataframe multi-index
+            if datatype == 'ensemble':
+                txt = txt.replace('# Scenarios;', 'Scenarios;')
+                txt = txt.replace('# DtDerObs;', 'DtDerObs;')
+            elif datatype == 'tendance':
+                txt = txt.replace('# Tendances;', 'Tendances;')
+                txt = txt.replace('# DtDerObs;', 'DtDerObs;')
 
-        # flatten nested column multi-index into simple multi-index
-        df0.columns = pd.MultiIndex.from_tuples(
-            [tuple([c[0], c[1], *c[2]]) for c in df0.columns],
-            names=[
-                'entités', 'variables', 'date émission',
-                'modèles', 'membres', 'probabilités'
-            ]
+        # get dataframe from text
+        df0 = pd.read_csv(
+            io.StringIO(txt),
+            sep=';',
+            comment='#',
+            header=[0, 1, 2, 3, 4],
+            index_col=0,
+            parse_dates=True,
+            date_format='%d-%m-%Y %H:%M',
+            na_values=(-99.900, '-99.900', -999.999, '-999.999'),
+            keep_default_na=True,
         )
 
         # drop irrelevant levels for `evalhyd`
-        df0 = df0.droplevel(('variables', 'modèles', 'probabilités'), axis=1)
+        df0 = df0.droplevel(('Grandeurs', 'IdSeries'), axis=1)
 
-        # give name to current row index
-        df0.index.names = ['date validité']
+        # rename indexes corresponding to `evalhyd` dimensions
+        df0.columns = df0.columns.rename(
+            {
+                'Stations': 'entités',
+                'Tendances': 'membres',
+                'Scenarios': 'membres',
+                'DtDerObs': 'date émission',
+            }
+        )
+        df0.index.name = 'date validité'
 
         # move column multi-index levels to row multi-index levels
-        df0 = df0.stack(df0.columns.names, future_stack=True).to_frame('valeur')
+        df0 = df0.stack(
+            df0.columns.names, future_stack=True
+        ).to_frame('valeur')
+
+        # parse issue dates to timestamp
+        df0.index = df0.index.set_levels(
+            pd.to_datetime(
+                df0.index.levels[df0.index.names.index('date émission')],
+                format='%d-%m-%Y %H:%M'
+            ),
+            level='date émission'
+        )
 
         # compute leadtimes from validity dates and issue dates
         df0.loc[:, 'échéances'] = (
-                df0.index.get_level_values('date validité')
-                - df0.index.get_level_values('date émission')
+            df0.index.get_level_values('date validité')
+            - df0.index.get_level_values('date émission')
         )
 
         # introduce new level in row multi-index for leadtimes
